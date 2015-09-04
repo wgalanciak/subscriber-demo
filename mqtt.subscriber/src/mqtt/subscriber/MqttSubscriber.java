9dbe1efadbdece76733033b3e6272fb9b44e0d28
Wojciech Galanciak <wojtek@genuitec.com>
2015-09-04 12:47:18 +0200

    update topic

--- mqtt.subscriber/src/mqtt/subscriber/MqttSubscriber.java
+++ mqtt.subscriber/src/mqtt/subscriber/MqttSubscriber.java
@@ -8,8 +8,11 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 public class MqttSubscriber {
 
 	public static final String BROKER_URL = "tcp://broker.mqttdashboard.com:1883";
+	
+	private static final String TOPIC = "home/temperature";
 
-	String clientId = Utils.getMacAddress() + "-sub";
+	private String clientId = Utils.getMacAddress() + "-sub";
+	
 	private MqttClient mqttClient;
 
 	public MqttSubscriber() {
@@ -23,21 +26,10 @@ public class MqttSubscriber {
 
 	public void start() {
 		try {
-
 			mqttClient.setCallback(new SubscribeCallback());
 			mqttClient.connect();
-
-			// Subscribe to all subtopics of home
-			final String topic = "home/temperature";
-			mqttClient.subscribe(topic);
-
-			System.out.println("Subscriber is now listening to " + topic);
-
-			final String topicLWT = "home/LWT";
-			mqttClient.subscribe(topicLWT);
-
-			System.out.println("Subscriber is now listening to " + topicLWT);
-
+			mqttClient.subscribe(TOPIC);
+			System.out.println("Subscriber is now listening to " + TOPIC);
 		} catch (MqttException e) {
 			e.printStackTrace();
 			System.exit(1);
